c82c3e810ca95238d594fe7e211235a4ecf43c90
rladmsghtkfkdgo <hearang3180@naver.com>
2026-03-13 16:36:42 +0900

    데이터베이스숙제 끝

--- 2026-1/DataBase/my_people.sql
+++ 2026-1/DataBase/my_people.sql
@@ -1,12 +1,4 @@
-.open 유명인DB.sqlite3
-DROP TABLE IF EXISTS 연예인;
-create table 연예인(이름 varchar(10), 키 int, 나이 int, 몸무게 int);
-INSERT INTO 연예인 VALUES('강호동',180,50,90);
-INSERT INTO 연예인 VALUES('유재석',178,48,70);
 
-SELECT * FROM 연예인;
-
-.open practice.db;
 DROP TABLE IF EXISTS my_people;
 create table my_people(name varchar(10),hight int,weight int,age int,sex varchar(10),major varchar(10));
 INSERT INTO my_people (name,hight,weight,age,sex,major)
